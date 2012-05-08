1e804b0a6cb5db1cd0fe068e0e28844f74d20631
Raidok <RKalbre@gmail.com>
2012-05-08 15:30:21 +0300

    bind new action with a handler

--- src/klik/server/guice/GuiceServerModule.java
+++ src/klik/server/guice/GuiceServerModule.java
@@ -1,6 +1,8 @@
 package klik.server.guice;
 
+import klik.server.handler.RetrieveSetupHandler;
 import klik.server.handler.SendGreetingHandler;
+import klik.shared.rpc.RetrieveSetupAction;
 import klik.shared.rpc.SendGreeting;
 
 import org.apache.commons.logging.Log;
@@ -13,6 +15,7 @@ public class GuiceServerModule extends HandlerModule {
 	@Override
 	protected void configureHandlers() {
 		bindHandler(SendGreeting.class, SendGreetingHandler.class);
+		bindHandler(RetrieveSetupAction.class, RetrieveSetupHandler.class);
 		bind(Log.class).toProvider(LogProvider.class).in(Singleton.class);
 	}
 
