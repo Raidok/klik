c35e814618610182a63a25d1aa04644323737d29
Raidok <RKalbre@gmail.com>
2012-05-10 09:40:37 +0300

    generated unit and a class of enums

---
+++ src/klik/shared/X10.java
@@ -0,0 +1,23 @@
+package klik.shared;
+
+public class X10 {
+
+	private X10() {
+	}
+
+	public enum Type {
+		UNIT, LIGHT
+	}
+
+	public enum HouseCode {
+		A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P
+	}
+
+	public enum UnitCode {
+		U1,U2,U3,U4,U5,U6,U7,U8,U9,U10,U11,U12,U13,U14,U15,U16
+	}
+
+	public enum State {
+		ON, OFF, DIM
+	}
+}
---
+++ src/klik/shared/model/Unit.java
@@ -0,0 +1,16 @@
+package klik.shared.model;
+
+import klik.shared.X10;
+
+import com.gwtplatform.dispatch.annotation.GenDto;
+import com.gwtplatform.dispatch.annotation.Order;
+
+@GenDto
+public class Unit {
+
+	@Order(1) X10.Type type;
+	@Order(2) X10.HouseCode houseCode;
+	@Order(3) X10.UnitCode unitCode;
+	@Order(4) X10.State state;
+
+}
