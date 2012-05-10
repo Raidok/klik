a4b8b4f93b41cd074108e4dd76f6a3fbe5d0e051
Raidok <RKalbre@gmail.com>
2012-05-10 13:44:23 +0300

    PropertiesManager submap and delete

--- src/klik/server/PropertiesManager.java
+++ src/klik/server/PropertiesManager.java
@@ -5,11 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
+
+import com.google.gwt.dev.util.collect.HashMap;
 
 /**
  * This class handles the server properties so they won't
- * disappear when restarting the app.
+ * disappear when restarting the app. Kind of a database..
  * @author raido
  */
 public class PropertiesManager {
@@ -20,6 +23,11 @@ public class PropertiesManager {
 	private PropertiesManager() {
 	}
 
+	/**
+	 * Loads value of the given key from the properties file
+	 * @param key
+	 * @return value stored in the file
+	 */
 	public static String getProperty(String key) {
 		ensureLoaded();
 		String value = properties.getProperty(key);
@@ -27,10 +35,43 @@ public class PropertiesManager {
 		return value;
 	}
 
+	/**
+	 * Saves the given key-value pair to the properties file
+	 * @param key
+	 * @param value
+	 */
 	public static void setProperty(String key, String value) {
 		ensureLoaded();
 		properties.setProperty(key, value);
 		storeProperties();
+	}
+
+	/**
+	 * Removes the given key from the file.
+	 * @param key
+	 */
+	public static void removeProperty(String key) {
+		ensureLoaded();
+		properties.remove(key);
+		storeProperties();
+	}
+
+	/**
+	 * Gets the submap of the given prefix and cuts it off from the
+	 * @param prefix
+	 */
+	public static HashMap<String, String> getSubMap(String prefix) {
+		ensureLoaded();
+		HashMap<String, String> map = new HashMap<String, String>();
+		for (Map.Entry<Object, Object> e : properties.entrySet()) {
+			if (e.getKey() instanceof String && e.getValue() instanceof String) {
+				if (((String) e.getKey()).indexOf(prefix) == 0) {
+					String key = ((String) e.getKey()).substring(prefix.length());
+					map.put(key, (String) e.getValue());
+				}
+			}
+		}
+		return map;
 	}
 
 	private static void ensureLoaded() {
