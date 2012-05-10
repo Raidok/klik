d2d126f995d5be8d5833864a10ecb25ac552c15c
Raidok <RKalbre@gmail.com>
2012-05-10 23:36:54 +0300

    unitevent returns status

--- src/klik/shared/rpc/UnitEvent.java
+++ src/klik/shared/rpc/UnitEvent.java
@@ -1,14 +1,17 @@
 package klik.shared.rpc;
 
 import klik.shared.model.UnitEventDto;
+import klik.shared.model.UnitStatusDto;
 
 import com.gwtplatform.dispatch.annotation.GenDispatch;
 import com.gwtplatform.dispatch.annotation.In;
+import com.gwtplatform.dispatch.annotation.Out;
 import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
 
 @GenDispatch(isSecure=false, serviceName=UnsecuredActionImpl.DEFAULT_SERVICE_NAME)
 public class UnitEvent {
 
 	@In(1) UnitEventDto event;
+	@Out(1) UnitStatusDto status;
 
 }
