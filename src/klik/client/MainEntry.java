c1c8960f198d19a90fb9cb01f0d230afd6967ca2
Raidok <RKalbre@gmail.com>
2012-05-22 12:31:09 +0300

    UncaughtExceptionHandler

--- src/klik/client/MainEntry.java
+++ src/klik/client/MainEntry.java
@@ -2,21 +2,28 @@ package klik.client;
 
 import klik.client.gin.MyGinjector;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.GWT.UncaughtExceptionHandler;
 import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class MainEntry implements EntryPoint {
+public class MainEntry implements EntryPoint, UncaughtExceptionHandler {
 	public final MyGinjector ginjector = GWT.create(MyGinjector.class);
 
 	@Override
 	public void onModuleLoad() {
-
+		GWT.setUncaughtExceptionHandler(this);
 		DelayedBindRegistry.bind(ginjector);
 		ginjector.getCss();
 		ginjector.getPlaceManager().revealCurrentPlace();
+	}
+
+	@Override
+	public void onUncaughtException(Throwable e) {
+		Log.error(e.getMessage(), e);
 	}
 }
